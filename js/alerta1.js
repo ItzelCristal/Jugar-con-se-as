39ad23c0d7c045eae72fe2ef88a412cbfadb9f00
IC <82540863+ItzelCristal@users.noreply.github.com>
2021-12-05 17:57:21 -0700

    Add files via upload

---
+++ js/alerta1.js
@@ -0,0 +1,29 @@
+/*
+Swal.fire({
+
+
+    width: 900,
+
+
+    html: '<link rel="stylesheet" href="estilosalert.css"></link>' +
+        '<h1>¡HOLA!</h1>  ' +
+        '<b>Bienvenido a "JUGAR CON SEÑAS"</b> ' +
+        '<div class="v1"> <video src="alertamultimedia/JCS1.webm" controls></video></div>',
+    showCloseButton: true,
+
+
+})  '<img src=alertamultimedia/alerta.gif  < /img><b></b > ',*/
+
+
+Swal.fire({
+
+    width: 700,
+    height: 400,
+
+    background: '#fff ',
+    html: '<link rel="stylesheet" href="estilosalert.css"></link>' +
+        '<h1>¡HOLA!</h1>  ' +
+        '<b>Bienvenido a "JUGAR CON SEÑAS"</b> ' +
+        '<div></div><video src="alertamultimedia/JCS1.mp4" width="500" height="300" controls poster="alertamultimedia/play.gif"></video> </div>',
+    showCloseButton: true,
+})
